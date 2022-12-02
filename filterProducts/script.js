48bd3b2b487009a2d68be68d7ee1f63e59c6106f
ArsenLen <laurie10@mail.ru>
2022-12-02 14:57:17 +0600

    js filterProducts

--- filterProducts/script.js
+++ filterProducts/script.js
@@ -49,3 +49,33 @@ const laptops = [
         category: "computer"
     },
 ]
+
+/*
+<div class="product">
+    <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true" 
+        alt="" class="product-img">
+    <h5 class="product-name">HP Dragon</h5>
+    <p class="product-price">15000</p>
+</div>
+*/
+
+const productsContainer = document.querySelector(".products")
+
+const showProducts = (products) => {
+    productsContainer.innerHTML = products.map(
+        product => `
+        <div class="product">
+            <img 
+                src="${product.img}" 
+                alt="" 
+                class="product-img">
+            <h5 class="product-name">${product.name}</h5>
+            <p class="product-price">${product.price}</p>
+        </div>
+        `
+    ).join("")
+}
+
+showProducts(laptops)
+
+// Повесить слушатель события на найденную картинку.
