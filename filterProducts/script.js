2706c419a1a39c28bb35693ff0963d64bc02b361
ArsenLen <laurie10@mail.ru>
2022-12-02 14:37:18 +0600

    data

---
+++ filterProducts/script.js
@@ -0,0 +1,51 @@
+const laptops = [
+    {
+        id: 1,
+        name: "Acer Paradise",
+        img: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true",
+        price: 20000,
+        category: "laptop"
+    },
+    {
+        id: 2,
+        name: "HP Pavillon",
+        img: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true",
+        price: 25000,
+        category: "laptop"
+    },
+    {
+        id: 3,
+        name: "Samsung X20",
+        img: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true",
+        price: 40000,
+        category: "monoblock"
+    },
+    {
+        id: 4,
+        name: "Iphone 13Pro",
+        img: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true",
+        price: 70000,
+        category: "smartphone"
+    },
+    {
+        id: 5,
+        name: "Sony TI201",
+        img: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true",
+        price: 55000,
+        category: "smartphone"
+    },
+    {
+        id: 6,
+        name: "Samsung",
+        img: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true",
+        price: 80000,
+        category: "computer"
+    },
+    {
+        id: 7,
+        name: "Acer Computer M500",
+        img: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LqQX?ver=1f00&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true",
+        price: 75000,
+        category: "computer"
+    },
+]
